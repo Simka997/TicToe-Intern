ec18b11516ac0f85f238c178d27597698508f27e
simka997 <simomio997@gmail.com>
2020-12-14 12:49:10 +0100

    najnovije sign

--- src/com/interw/PlayerSign.java
+++ src/com/interw/PlayerSign.java
@@ -3,5 +3,6 @@ package com.interw;
 public class PlayerSign {
     public static final char X_PLAYER_SIGN = 'Y';
     public static final char O_PLAYER_SIGN = 'O';
+    public static final char D_PLAYER_SIGN = 'O';
 
 }

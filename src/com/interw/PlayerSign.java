4a23669b10c46073c1b444e0a115166ae675dfb2
simka997 <simomio997@gmail.com>
2020-12-14 13:13:30 +0100

    head V

--- src/com/interw/PlayerSign.java
+++ src/com/interw/PlayerSign.java
@@ -4,5 +4,6 @@ public class PlayerSign {
     public static final char X_PLAYER_SIGN = 'Y';
     public static final char O_PLAYER_SIGN = 'O';
     public static final char M_PLAYER_SIGN = 'S';
+    public static final char V_PLAYER_SIGN = 'S';
 
 }

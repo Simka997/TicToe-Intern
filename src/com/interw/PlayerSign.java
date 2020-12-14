5494dc33890378ed3349f64f272f9f71ad64b658
simka997 <simomio997@gmail.com>
2020-12-14 12:08:31 +0100

    Novo znma

--- src/com/interw/PlayerSign.java
+++ src/com/interw/PlayerSign.java
@@ -2,6 +2,6 @@ package com.interw;
 
 public class PlayerSign {
     public static final char X_PLAYER_SIGN = 'X';
-    public static final char O_PLAYER_SIGN = 'O';
+    public static final char O_PLAYER_SIGN = 'Z';
 
 }
